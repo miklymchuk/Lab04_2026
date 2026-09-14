bd343496132dc062d3e203a7bd0a6548f1cefdc3
miklymchuk <miklymchuk@gmail.com>
2026-09-14 09:19:05 -0400

    Added Labels, TextFields & a confirmation Button for information the user must provide & information on travel expenses (total expenses, reimbursement, excess, amount saved).

--- src/main/java/com/mycompany/lab04_2026/App.java
+++ src/main/java/com/mycompany/lab04_2026/App.java
@@ -3,6 +3,8 @@ package com.mycompany.lab04_2026;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,9 +15,43 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var label = new Label("label");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    public void start(Stage stage) {       
+        var root = new GridPane();
+        
+        // Fields to fill for the business trip.
+        var days = new Label("Number of days:");
+        var days2 = new TextField();
+        
+        var airfare = new Label("Airfare:");
+        var airfare2 = new TextField();
+        
+        var rental = new Label("Car rental fees:");
+        var rental2 = new TextField();
+        
+        var miles = new Label("Miles Driven:");
+        var miles2 = new TextField();
+        
+        var parking = new Label("Parking fees:");
+        var parking2 = new TextField();
+        
+        var taxi = new Label("Taxi charges:");
+        var taxi2 = new TextField();
+        
+        var conference = new Label("Conference or seminar charges:");
+        var conference2 = new TextField();
+        
+        var lodging = new Label("Lodging charges:");
+        var lodging2 = new TextField();
+        
+        var confirmation = new Button("Confirm");
+        
+        // Data (cost & reimbursement) of the business trip
+        var expenses = new Label("");
+        var allowable = new Label("");
+        var excess = new Label("");
+        var moneySaved = new Label("");
+        
+        var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
@@ -23,5 +59,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
