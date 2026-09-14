d04ca3a12ee4e5951d84420a87fe418d89d01651
miklymchuk <miklymchuk@gmail.com>
2026-09-14 09:28:09 -0400

    Added the Labels, TextFields & Confirmation Button to the GridPane and displayed it on the scene.

--- src/main/java/com/mycompany/lab04_2026/App.java
+++ src/main/java/com/mycompany/lab04_2026/App.java
@@ -2,6 +2,7 @@ package com.mycompany.lab04_2026;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -15,7 +16,7 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {       
+    public void start(Stage stage) {     
         var root = new GridPane();
         
         // Fields to fill for the business trip.
@@ -50,6 +51,30 @@ public class App extends Application {
         var allowable = new Label("");
         var excess = new Label("");
         var moneySaved = new Label("");
+        
+        // Adding fields to fill & data to the root GridPane
+        root.add(days, 0, 0);
+        root.add(days2, 1, 0);
+        root.add(airfare, 0, 1);
+        root.add(airfare2, 1, 1);
+        root.add(rental, 0, 2);
+        root.add(rental2, 1, 2);
+        root.add(miles, 0, 3);
+        root.add(miles2, 1, 3);
+        root.add(parking, 0, 4);
+        root.add(parking2, 1, 4);
+        root.add(taxi, 0, 5);
+        root.add(taxi2, 1, 5);
+        root.add(conference, 0, 6);
+        root.add(conference2, 1, 6);
+        root.add(lodging, 0, 7);
+        root.add(lodging2, 1, 7);
+        root.add(confirmation, 1, 8);
+        root.add(expenses, 0, 9);
+        root.add(allowable, 0, 10);
+        root.add(excess, 0, 11);
+        root.add(moneySaved, 0, 12);
+        
         
         var scene = new Scene(root, 640, 480);
         stage.setScene(scene);
